deafad346d72cbd57ee9cc6986a74a7332470595
tpaphysics <physics.thiago@gmail.com>
2022-06-13 08:09:43 -0300

    feat: instalação e configuração das fontes, criação da pasta theme, adição da tabela de cores

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
+import { useFonts, RobotoSlab_400Regular } from '@expo-google-fonts/roboto-slab';
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    RobotoSlab_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ fontFamily: 'RobotoSlab_400Regular' }}>
+        Open up App.tsx to start workin on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ theme/fonts.ts
@@ -0,0 +1,7 @@
+export default {
+  regular: 'RobotoSlab_400Regular',
+  medium: 'RobotoSlab_500Medium',
+  semiBold: 'RobotoSlab_600SemiBolder',
+  bold: 'RobotoSlab_700Bold',
+  extraBold: 'RobotoSlab_800ExtraBolber',
+};
